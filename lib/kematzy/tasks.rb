877ca057e014b5b636786a8860be31cb0b3c2318
kematzy <kematzy@gmail.com>
2009-11-20 00:33:42 +0800

    Renamed default file to follow my structure

---
+++ lib/kematzy/tasks.rb
@@ -0,0 +1,13 @@
+
+
+module Kematzy
+  
+  module Tasks
+    VERSION = '0.1.0'
+    def self.version
+      "Kematzy::Tasks v#{VERSION}"
+    end
+    
+  end #/module Tasks
+  
+end #/module Kematzy
