f9fc25499cc57de88860c8d6af946940e388892e
kematzy <kematzy@gmail.com>
2010-02-22 16:37:59 +0800

    Added informative RSpec test ;-)

---
+++ spec/kematzy/tasks_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
+
+describe "Kematzy" do 
+  
+  describe "Tasks" do 
+    
+    it "should have some real tests where all the functionality is carefully tested" do
+      "If You know how to do this, please fork and show me ;-)".should be_a_kind_of(String)
+    end
+    
+  end #/ Tasks
+  
+end #/ Kematzy
