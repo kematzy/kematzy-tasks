e048ffa6d936018bdae456550edf6f30275ede04
kematzy <kematzy@gmail.com>
2010-03-15 18:07:43 +0800

    Fix of Symlink Tasks.

--- lib/kematzy/tasks/rake/symlink.rb
+++ lib/kematzy/tasks/rake/symlink.rb
@@ -5,11 +5,12 @@ namespace :symlink do
   task :shared, [:dirs] do |t, args|
     unless args.dirs
       msg = %Q[\nERROR:\n\n  You must define the :dirs variable like this:\n]
-      msg << %Q[  rake symlink:shared dirs=assets, downloads, images, photos, movies, system, uploads,... \n\n]
+      msg << %Q[  rake symlink:shared dirs="assets, downloads, images, photos, movies, system, uploads,..." \n\n]
       puts msg
     else
-      PROJECT_ROOT = File.dirname(File.expand_path(__FILE__))
-      dirs.each do |dir|
+      PROJECT_ROOT = File.expand_path(Dir.pwd)
+      args.dirs.split(',').each do |dir|
+        dir = dir.strip
         `ln -s #{File.join(File.dirname(PROJECT_ROOT), 'shared', dir)} #{File.join(PROJECT_ROOT, 'public', dir)}`
         puts "symlinked  '../shared/#{dir}' => 'current/public/#{dir}'"
       end
@@ -17,16 +18,3 @@ namespace :symlink do
   end
   
 end #/ namespace symlink
-# 
-# namespace :symlink do
-#   
-#   desc "Symlink shared directories to current/public"
-#   task :shared do 
-#     PROJECT_ROOT = File.dirname(File.expand_path(__FILE__))
-#     %w(assets downloads images photos movies system uploads).each do |dir|
-#       `ln -s #{File.join(File.dirname(PROJECT_ROOT), 'shared', dir)} #{File.join(PROJECT_ROOT, 'public', dir)}`
-#       puts "symlinked  '../shared/#{dir}' => 'current/public/#{dir}'"
-#     end
-#   end
-#   
-# end #/ namespace symlink
